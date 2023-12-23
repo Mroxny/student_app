84df300bb6abe6895c6407c27592217bee927f52
Mroxny <50219282+Mroxny@users.noreply.github.com>
2023-12-23 14:41:37 +0100

    Add serialisation

--- app/src/main/java/com/example/student_app/Student.kt
+++ app/src/main/java/com/example/student_app/Student.kt
@@ -1,5 +1,8 @@
 package com.example.student_app
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 class Student(var name:String, var age:Int, var gpa:Float) {
     fun canPass(): Boolean {
         return this.gpa >= 3
